c8058ebe940fbae5d92cff58d176099c64ae7e18
Ubuntu <ubuntu@ubuntu1804.linuxvmimages.local>
2021-07-18 19:57:48 -0400

    Updating scripts

---
+++ install_docker.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo ">>> Installing docker"
+sudo apt update
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release \
+    apt-transport-https \ 
+    ca-certificates \
+    software-properties-common
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt-get update
+sudo apt-cache policy docker-ce
+sudo apt-get install -y nodejs docker-ce
+sudo usermod -aG docker ${USER}
+su - ${USER}
--- setup.sh
+++ setup.sh
@@ -3,17 +3,8 @@
 echo ">>> Setup started"
 echo ">>> Installing requirements"
 sudo apt update
-sudo apt-get remove -y docker docker-engine docker.io containerd runc
-sudo apt-get install -y \
-    git
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-sudo apt-get update
-sudo apt-get install -y nodejs docker-ce docker-ce-cli containerd.io
+sudo apt-get install -y git
+    
 echo ">>> Cloning the repo"
 sudo mkdir -p ./dm/front && sudo mkdir -p ./dm/back \
     && sudo chown $USER:$USER ./dm/front \
