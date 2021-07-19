c7af1867a397d6e2e61eb454ace03799ed8867d9
isandroazedo <isandro.azedo@gmail.com>
2021-07-18 20:44:20 -0400

    Updating setup.sh

--- setup.sh
+++ setup.sh
@@ -2,30 +2,26 @@
 
 echo ">>> Setup started"
 echo ">>> Installing requirements"
-
-sudo apt update && curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash - \
-    && sudo apt install -y nodejs nginx mysql-server \
-    && sudo mysql_secure_installation && && systemctl status mysql.service \
-    && sudo ufw app list && sudo ufw allow 'Nginx HTTP' \
-    && sudo cp dm /etc/nginx/sites-available/ \
-    && sudo ln -s /etc/nginx/sites-available/dm /etc/nginx/sites-enabled/ \
-    && sudo nginx -t && sudo systemctl restart nginx \
-    && sudo npm install typescript -g \
-    && sudo mysql.server start \
-    && sudo mysql -u root -e "SET PASSWORD FOR root@'127.0.0.1' = PASSWORD('pwd1212');"
-
-sudo npm install npm@latest -g && sudo npm install -g @angular/cli && sudo npm install --global Yarn
-
-sudo mkdir -p ~/git/dm/front && sudo mkdir -p ~/git/dm/back \
-    && git clone https://github.com/isandroazedo/device-management-web.git ~/git/dm/front \
-    && git clone https://github.com/isandroazedo/device-management-api.git ~/git/dm/back
-
-echo "127.0.0.1  dm" >> /etc/hosts
-
-cd ~/git/dm/web
-export NODE_ENV=development && sudo yarn install && sudo yarn build && sudo cp -R config build/config/ \
-    && sudo cp -R dist/device-management-web/*.* /var/www/dm/html/ && sudo systemctl restart nginx
-
-cd ~/git/dm/back
-export NODE_ENV=development && sudo yarn install && sudo yarn build && sudo cp -R config build/config \
-    && npx sequelize-cli db:migrate && nohup node build/index.js &
+sudo apt update
+sudo apt-get remove -y docker docker-engine docker.io containerd runc
+sudo apt-get install -y \
+    git
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+sudo apt-get update
+sudo apt-get install -y nodejs docker-ce docker-ce-cli containerd.io
+echo ">>> Cloning the repo"
+sudo mkdir -p ./dm/front && sudo mkdir -p ./dm/back \
+    && sudo chown $USER:$USER ./dm/front \
+    && sudo chown $USER:$USER ./dm/back \
+    && git clone https://github.com/isandroazedo/device-management-web.git ./dm/front \
+    && git clone https://github.com/isandroazedo/device-management-api.git ./dm/back \
+    && cd ./dm/back \
+    && make api-base-image \
+    && make api-image \
+    && cd ../front \
+    && make image
