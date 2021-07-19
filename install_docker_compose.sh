977afff329039977787977255ab37f12398d5206
Ubuntu <ubuntu@ubuntu1804.linuxvmimages.local>
2021-07-18 20:41:40 -0400

    Fixing scripts

--- install_docker_compose.sh
+++ install_docker_compose.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
-echo ">>> Installing docker docmpose"
-sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+echo ">>> Installing docker compose"
+sudo apt-get install -y curl \
+&& sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose \
+&& sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
